eb1e7689bbcaf29f9c4480903e03ad7c628f3f5f
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-09-17 11:56:26 +0700

    Add layout dan source file tahap 1

--- app/src/main/java/com/uc/hans_0706011910007/MainActivity.java
+++ app/src/main/java/com/uc/hans_0706011910007/MainActivity.java
@@ -1,6 +1,6 @@
 package com.uc.hans_0706011910007;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
