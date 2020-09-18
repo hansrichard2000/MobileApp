47deea136009a38a3ff0af6512f67335e4ee4747
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-09-18 22:16:05 +0700

    Revert "Coding untuk bisa intent data ke card view"
    This reverts commit 3e842450

--- app/src/main/java/com/uc/hans_0706011910007/UserDetailActivity.java
+++ app/src/main/java/com/uc/hans_0706011910007/UserDetailActivity.java
@@ -2,29 +2,20 @@ package com.uc.hans_0706011910007;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
 
-import com.uc.hans_0706011910007.dialog.DeleteActivity;
-import com.uc.hans_0706011910007.model.SimpanData;
 import com.uc.hans_0706011910007.model.User;
-
-import java.util.ArrayList;
 
 public class UserDetailActivity extends AppCompatActivity {
 
-    private static final String TAG ="try";
     TextView fname, age, address;
     Toolbar toolbar;
-    ImageButton edit, delete;
-    ArrayList<User>listUsers = SimpanData.listUsers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,49 +24,37 @@ public class UserDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        fname = findViewById(R.id.fname2);
-        age = findViewById(R.id.age2);
-        address = findViewById(R.id.address2);
-
-        Intent intent = getIntent();
-        final int position = intent.getIntExtra("pos", 0);
-        User user = intent.getParcelableExtra("listUsers");
-//        Log.d(TAG, "onCreate:" + user.toString());
-
-        fname.setText(user.getFname());
-        age.setText(user.getAge()+" Years Old");
-        address.setText(user.getAddress());
-
-        edit = findViewById(R.id.button_edit);
-        delete = findViewById(R.id.button_delete);
-        edit.setOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(UserDetailActivity.this, AddUserActivity.class);
-                intent.putExtra("listUsers", listUsers.get(position));
-                intent.putExtra("position", position);
+                Intent intent = new Intent(UserDetailActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                deleteDialog();
-            }
-        });
+        fname = findViewById(R.id.fname);
+        age = findViewById(R.id.age2);
+        address = findViewById(R.id.address2);
+
+        Intent intent = getIntent();
+        User user = intent.getParcelableExtra("listusers");
+
+        fname.setText(user.getFname());
+        age.setText(user.getAge());
+        address.setText(user.getAddress());
     }
 
-    private void deleteDialog() {
-        DeleteActivity deleteActivity;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onBackPressed(){
-        Intent intent = new Intent(UserDetailActivity.this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
-        finish();
+        NavUtils.navigateUpFromSameTask(this);
+
     }
 }
